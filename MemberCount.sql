29c972fa58d514703bf72376e5bc10da1071b5ef
StevenCyrway <stevencyrway@gmail.com>
2021-12-10 17:24:06 -0600

    assembling query to identify campaign stats easily.

--- MemberCount.sql
+++ MemberCount.sql
@@ -6,5 +6,59 @@ Select count(distinct signups.northstar_id) as Signups,
 from quasar_prod_warehouse.public.signups
          join quasar_prod_warehouse.public.reportbacks on signups.campaign_id = reportbacks.campaign_id
          join quasar_prod_warehouse.public.campaign_info on signups.campaign_id = cast(campaign_info.campaign_id as varchar)
-where signups.campaign_id in ('9134','9136','9135','9133')
-group by campaign_info.campaign_id, campaign_info.campaign_name, campaign_info.campaign_created_date
+where signups.campaign_id in ('9136')
+group by campaign_info.campaign_id, campaign_info.campaign_name, campaign_info.campaign_created_date;
+
+
+Select campaign_info.campaign_id,
+       campaign_info.campaign_run_id,
+       campaign_name,
+       campaign_cause,
+       campaign_run_start_date,
+       campaign_run_end_date,
+       campaign_created_date,
+       campaign_node_id,
+       group_type_id,
+       contentful_id,
+       contentful_internal_title,
+       contentful_title,
+       contentful_raf_flag,
+       campaign_node_id_title,
+       campaign_run_id_title,
+       campaign_action_type,
+       campaign_cause_type,
+       campaign_noun,
+       campaign_verb,
+       campaign_cta,
+       action_types,
+       online_offline,
+       scholarship,
+       post_types,
+       northstar_id,
+       id,
+       campaign_info.campaign_id,
+       campaign_info.campaign_run_id,
+       club_id,
+       why_participated,
+       source,
+       details,
+       referrer_user_id,
+       group_id,
+       source_bucket,
+       created_at,
+       source_details,
+       utm_medium,
+       utm_source,
+       utm_campaign,
+       signup_rank,
+       count(distinct signups.northstar_id) as Signups
+from campaign_info
+         join quasar_prod_warehouse.public.signups on cast(signups.campaign_id as bigint) = campaign_info.campaign_id
+group by campaign_info.campaign_id, campaign_info.campaign_run_id, campaign_name, campaign_cause,
+         campaign_run_start_date, campaign_run_end_date, campaign_created_date, campaign_node_id, group_type_id,
+         contentful_id, contentful_internal_title, contentful_title, contentful_raf_flag, campaign_node_id_title,
+         campaign_run_id_title, campaign_action_type, campaign_cause_type, campaign_noun, campaign_verb, campaign_cta,
+         action_types, online_offline, scholarship, post_types, northstar_id, id, campaign_info.campaign_id, campaign_info.campaign_run_id, club_id,
+         why_participated, source, details, referrer_user_id, group_id, source_bucket, created_at, source_details,
+         utm_medium, utm_source, utm_campaign, signup_rank
+order by campaign_run_start_date desc
--- Test.sql
+++ Test.sql
@@ -1,100 +1,68 @@
-WITH signups AS (select s.northstar_id,
-                s.id,
-                s.club_id,
-                s.campaign_id,
-                s.campaign_run_id,
-                s.why_participated,
-                s.source,
-                s.source_bucket,
-                s.source_details,
-                s.created_at,
-                s.utm_source,
-                s.utm_medium,
-                s.utm_campaign,
-                s.referrer_user_id,
-                s.group_id,
-                p1.ignore,
-                s.details
-                --,
-                --r.post_created_at post_date,
-                --date_part('day', r.post_created_at - s.created_at) as num_days_rb
-          from public.signups s
-          left join (
-              select distinct northstar_id, campaign_id, 1 as ignore
-              from public.posts
-              where signup_id = -1
-          ) p1 on s.northstar_id = p1.northstar_id and s.campaign_id = p1.campaign_id
-          --left join public.reportbacks r on s.id = r.signup_id
-          )
-SELECT
-    (DATE(campaign_info.campaign_run_start_date )) AS "campaign_info.campaign_run_start_date",
-    campaign_info.campaign_name  AS "campaign_info.campaign_name",
-    campaign_info.online_offline  AS "campaign_info.online_offline",
-    CASE WHEN campaign_info.scholarship ='Scholarship'
-      THEN TRUE ELSE FALSE END  AS "campaign_info.is_scholarship",
-    post_actions.noun  AS "post_actions.action_noun",
-    post_actions.verb  AS "post_actions.action_verb",
-        (CASE WHEN posts.is_reportback  THEN 'Yes' ELSE 'No' END) AS "posts.is_reportback",
-    COUNT(DISTINCT posts.northstar_id ) AS "posts.count_of_distinct_members_posting",
-    COALESCE(SUM(CASE
---         WHEN  posts.quantity   > 10000 THEN 1
-        WHEN  posts.quantity   IS NULL THEN 1
-             ELSE  posts.quantity   END  ), 0) AS "posts.quantity_clean"
-FROM signups
-LEFT JOIN public.posts  AS posts ON signups.id = posts.signup_id
-LEFT JOIN public.post_actions  AS post_actions ON posts.action_id = post_actions.id
-LEFT JOIN public.campaign_info  AS campaign_info ON signups.campaign_id = (campaign_info.campaign_id::varchar)
-WHERE (posts.status ) = 'accepted' AND (campaign_info.campaign_name ) LIKE '%Why We Should Care%'
-GROUP BY
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7
-ORDER BY
-    2 DESC
-FETCH NEXT 500 ROWS ONLY
+WITH first_rb AS (
+	SELECT
+		min(id) AS post_id
+	FROM
+		quasar_prod_warehouse.public.posts p
+	WHERE
+		p.is_reportback = 'true'
+		AND p.is_accepted = 1
+	GROUP BY
+		p.northstar_id,
+		p.campaign_id,
+		p.signup_id,
+		p.post_class,
+		p.reportback_volume
+	UNION
+	DISTINCT
+	SELECT
+		id
+	FROM
+		quasar_prod_warehouse.public.posts p
+	WHERE
+		p.is_reportback = 'true'
+		AND p.is_accepted = 1
+		AND TYPE = 'voter-reg'
+)
+SELECT sum(reportback_volume)
+-- 	pd.campaign_id,
+-- 	pd.club_id,
+-- 	pd.group_id,
+-- 	pd.id AS post_id,
+-- 	pd.is_civic_action,
+-- 	pd.is_scholarship_entry,
+-- 	pd.location,
+-- 	pd.northstar_id,
+-- 	pd."type" AS post_type,
+-- 	pd.post_class,
+-- 	pd.signup_id,
+-- 	pd.source AS post_source,
+-- 	pd.source_bucket AS post_source_bucket,
+-- 	pd.status AS post_status,
+-- 	pd.hours_spent as hours_spent,
+-- 	pd.created_at AS post_created_at,
+-- 	pd.postal_code,
+-- 	CASE
+-- 		WHEN (
+-- 			pd.post_class ILIKE '%vote%'
+-- 			AND pd.status = 'confirmed'
+-- 		) THEN 'self-reported registrations'
+-- 		WHEN (
+-- 			pd.post_class ILIKE 'voter-reg - %'
+-- 			AND pd.status <> 'confirmed'
+-- 		) THEN 'voter_registrations'
+-- 		WHEN pd."type" ILIKE '%photo%'
+-- 		AND pd.post_class NOT ILIKE '%vote%' THEN 'photo_rbs'
+-- 		WHEN pd."type" ILIKE '%text%' THEN 'text_rbs'
+-- 		WHEN pd."type" ILIKE '%social%' THEN 'social'
+-- 		WHEN pd."type" ILIKE '%call%' THEN 'phone_calls'
+-- 		ELSE NULL
+-- 	END AS post_bucket,
+-- 	pd.reportback_volume,
+-- 	pd.vr_source,
+-- 	pd.vr_source_details,
+-- 	pd.tags
 
--- sql for creating the total and/or determining pivot columns
-WITH signups AS (select s.northstar_id,
-                s.id,
-                s.club_id,
-                s.campaign_id,
-                s.campaign_run_id,
-                s.why_participated,
-                s.source,
-                s.source_bucket,
-                s.source_details,
-                s.created_at,
-                s.utm_source,
-                s.utm_medium,
-                s.utm_campaign,
-                s.referrer_user_id,
-                s.group_id,
-                p1.ignore,
-                s.details
-                --,
-                --r.post_created_at post_date,
-                --date_part('day', r.post_created_at - s.created_at) as num_days_rb
-          from public.signups s
-          left join (
-              select distinct northstar_id, campaign_id, 1 as ignore
-              from public.posts
-              where signup_id = -1
-          ) p1 on s.northstar_id = p1.northstar_id and s.campaign_id = p1.campaign_id
-          --left join public.reportbacks r on s.id = r.signup_id
-          )
-SELECT
-    COUNT(DISTINCT posts.northstar_id ) AS "posts.count_of_distinct_members_posting",
-    COALESCE(SUM(CASE
---         WHEN  posts.quantity   > 10000 THEN 1
-        WHEN  posts.quantity   IS NULL THEN 1
-             ELSE  posts.quantity   END  ), 0) AS "posts.quantity_clean"
-FROM signups
-LEFT JOIN public.posts  AS posts ON signups.id = posts.signup_id
-LEFT JOIN public.post_actions  AS post_actions ON posts.action_id = post_actions.id
-LEFT JOIN public.campaign_info  AS campaign_info ON signups.campaign_id = (campaign_info.campaign_id::varchar)
-WHERE (posts.status ) = 'accepted' AND (campaign_info.campaign_name ) LIKE '%Why We Should Care%'
-FETCH NEXT 1 ROWS ONLY
+FROM
+	quasar_prod_warehouse.public.posts pd
+	JOIN first_rb f ON (pd.id = f.post_id)
+where campaign_id = '9133'
