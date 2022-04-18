d0cf41b85084e55163a2e31dfd93b5cad9da79b9
steven cyrway <stevencyrway@gmail.com>
2022-04-18 10:21:19 -0500

    still making progress on building out the marketing and messaging queries to feed the dashboard.

--- MessagingQuerySMSEmail.sql
+++ MessagingQuerySMSEmail.sql
@@ -1,15 +1,16 @@
-------SMS funnel
-with sms_funnel as (
+-----Messaging funnel email & sms
+with messaging_funnel as (
     SELECT o.message_id           as event_id,
            o.user_id,
            o.created_at,
            o.macro,
            o.broadcast_id,
-           o.campaign_id,
+           cast(o.campaign_id as varchar),
            o.conversation_id,
            o.template             as content_type,
            --o.text as message_text,
-           'sms_outbound_message' as event_type
+           'sms_outbound_message' as event_type,
+           'SMS'                  as Modality
     FROM public.gambit_messages_outbound o
     UNION ALL
     SELECT i.message_id          as event_id,
@@ -17,11 +18,12 @@ with sms_funnel as (
            i.created_at,
            i.macro,
            i.broadcast_id,
-           i.campaign_id,
+           cast(i.campaign_id as varchar),
            i.conversation_id,
            i.template            as content_type,
            --i.text as message_text,
-           'sms_inbound_message' as event_type
+           'sms_inbound_message' as event_type,
+           'SMS'                 as Modality
     FROM public.gambit_messages_inbound i
     UNION ALL
     SELECT c.click_id                      as event_id,
@@ -38,64 +40,44 @@ with sms_funnel as (
            --null as message_text,
            CASE
                WHEN c.interaction_type IN ('click', 'uncertain') THEN 'sms_link_click'
-               ELSE 'sms_link_preview' END AS event_type
+               ELSE 'sms_link_preview' END AS event_type,
+           'SMS'                           as Modality
     FROM public.bertly_clicks c
     WHERE c.source = 'sms'
+    UNION ALL
+    SELECT event_id,
+           customer_id       as user_id,
+           timestamp         as created_at,
+           null              as macro,
+           null              as broadcast_id,
+           cio_campaign_id   as campaign_id,
+           email_id          as conversation_id,
+           cio_campaign_type as content_type,
+           event_type,
+           'Email'           as Modality
+    from cio_email_events
 )
 
-Select * from sms_funnel
+Select event_id,
+       user_id,
+       messaging_funnel.created_at,
+       macro,
+       broadcast_id,
+       campaign_id,
+       conversation_id,
+       content_type,
+       event_type,
+       Modality,
+       northstar_id,
+       last_logged_in,
+       last_accessed,
+       last_messaged_at,
+       email_subscription_topics,
+       voter_registration_status,
+       state,
+       country,
+       language
+from messaging_funnel
+         join users on user_id = northstar_id
+where messaging_funnel.created_at >= NOW() - INTERVAL '90 DAY'
 
--- Select count(distinct event_id)                                             as Events,
---        count(distinct user_id)                                              as Users,
---        count(distinct conversation_id)                                      as Messages,
---        date_trunc('week', created_at),
---        SUM(CASE WHEN content_type = 'email_opened' THEN 1 ELSE 0 END)       AS Opened,
---        SUM(CASE WHEN content_type = 'email_clicked' THEN 1 ELSE 0 END)      AS Clicked,
---        SUM(CASE WHEN content_type = 'email_bounced' THEN 1 ELSE 0 END)      AS Undeliverable,
---        SUM(CASE WHEN content_type = 'email_converted' THEN 1 ELSE 0 END)    AS Converted,
---        SUM(CASE WHEN content_type = 'email_unsubscribed' THEN 1 ELSE 0 END) AS Unsubscribed,
---        SUM(CASE WHEN content_type = 'email_sent' THEN 1 ELSE 0 END)         AS Sent,
---        'SMS'                                                                as Modality,
---        campaign_id,
---        content_type
--- from sms_funnel
--- where created_at >= (CURRENT_DATE + INTERVAL '-1 year')
--- group by date_trunc('week', created_at), Modality, campaign_id, content_type
-
-
----email funnel
-
-Select customer_id,
-       count(distinct customer_id)                                        as Users,
-       count(distinct email_id)                                           as Messages,
-       count(distinct event_id)                                           as Events,
-       SUM(CASE WHEN event_type = 'email_opened' THEN 1 ELSE 0 END)       AS Opened,
-       SUM(CASE WHEN event_type = 'email_clicked' THEN 1 ELSE 0 END)      AS Clicked,
-       SUM(CASE WHEN event_type = 'email_bounced' THEN 1 ELSE 0 END)      AS Undeliverable,
-       SUM(CASE WHEN event_type = 'email_converted' THEN 1 ELSE 0 END)    AS Converted,
-       SUM(CASE WHEN event_type = 'email_unsubscribed' THEN 1 ELSE 0 END) AS Unsubscribed,
-       SUM(CASE WHEN event_type = 'email_sent' THEN 1 ELSE 0 END)         AS Sent,
-       date_trunc('week', timestamp),
-       'Email'                                                            as Modality,
-       cio_campaign_type,
-       cio_campaign_id,
-       cio_campaign_name
-from cio_email_events
-where timestamp >= '2020-01-01'
-group by customer_id, date_trunc('week', timestamp), Modality, cio_campaign_type, cio_campaign_id, cio_campaign_name;
-
-
-
-
-Select * from gambit_messages_outbound;
-
-
-Select * from gambit_messages_inbound;
-
-
-select * from bertly_clicks
-
-
-select * from cio_email_events
-
-select * from campaign_info
